de9811fa8efcd9a0d516daef057c487c245e39b4
Ganesh07A <work.ganeshsuvarnkar@gmail.com>
2026-01-15 15:22:57 +0530

    Add StudentDashboard and ExamView components; update routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import TeacherDashboard from "./pages/TeacherDashboard"; // Import this
 import CreateExam from "./pages/CreateExam";
-
+import StudentDashboard from "./pages/StudentDashboard";
+import ExamView from "./pages/ExamView";
 function StudentPlaceholder() {
   return <h1 className="text-center mt-10 text-2xl">Student Dashboard (Coming Soon)</h1>;
 }
@@ -13,8 +14,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/teacher-dashboard" element={<TeacherDashboard />} /> {/* Use Component */}
-        <Route path="/student-dashboard" element={<StudentPlaceholder />} />
         <Route path="/create-exam" element={<CreateExam />} />
+        <Route path="/student-dashboard" element={<StudentDashboard/>} />
+        <Route path="/take-exam/:examId" element={<ExamView/>} />
       </Routes>
     </BrowserRouter>
   );
